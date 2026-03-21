fbbce995dc28980eca4d08a7301f175559f46c47
Rodney Kinney <rodneyk@allenai.org>
2026-03-21 11:54:46 -0700

    Add 'literature report' subcommand and use it for literature-report skill

--- src/asta/cli.py
+++ src/asta/cli.py
@@ -7,6 +7,7 @@ from asta.commands.auth import auth
 from asta.documents import documents
 from asta.experiment import experiment
 from asta.literature.find import find
+from asta.literature.report import report
 from asta.papers.author import author
 from asta.papers.citations import citations
 from asta.papers.get import get
@@ -46,6 +47,7 @@ cli.add_command(pdf_extraction)
 
 # Register literature subcommands
 literature.add_command(find)
+literature.add_command(report)
 
 # Register papers subcommands
 papers.add_command(get)
