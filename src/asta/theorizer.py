cbd13e5e867048e8aecadec638b9d3c6c0c1ee4c
Regan Huff <reganh@allenai.org>
2026-05-01 09:18:12 -0700

    adapt theorizer to updated asta-sdk version

--- src/asta/theorizer.py
+++ src/asta/theorizer.py
@@ -1,5 +1,6 @@
 from asta_agent.a2a.commands import make_a2a_group
 
+from asta.utils.auth_helper import get_access_token
 from asta.utils.config import get_api_config
 
 
@@ -10,9 +11,10 @@ def _theorizer_url() -> str:
 generate_theories = make_a2a_group(
     name="generate-theories",
     url_factory=_theorizer_url,
+    token_factory=get_access_token,
     help=(
         "Generate scientific theories via the Ai2 Theorizer agent.\n\n"
         "Subcommands talk to the Theorizer through asta-gateway. Auth comes from\n"
-        "`asta auth login` by default (or --api-key / $ASTA_A2A_API_KEY)."
+        "`asta auth login`."
     ),
 )
